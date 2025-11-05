fd7863bfa76596202eb8485111f8be2f5c3a8f2d
Chintalagiri Shashank <shashank@chintal.in>
2025-11-05 22:20:51 +0530

    Make the dummy cache async aware

--- src/tendril/caching/providers/dummy.py
+++ src/tendril/caching/providers/dummy.py
@@ -1,12 +1,19 @@
 
 
+import asyncio
 from functools import wraps
 
 
 def cache(namespace=None, ttl=None, key=None):
     def _wrapper_factory(func):
-        @wraps(func)
-        def wrapper_func(*args, **kwargs):
-            return func(*args, **kwargs)
-        return wrapper_func
+        if asyncio.iscoroutinefunction(func):
+            @wraps(func)
+            async def async_wrapper_func(*args, **kwargs):
+                return await func(*args, **kwargs)
+            return async_wrapper_func
+        else:
+            @wraps(func)
+            def sync_wrapper_func(*args, **kwargs):
+                return func(*args, **kwargs)
+            return sync_wrapper_func
     return _wrapper_factory
